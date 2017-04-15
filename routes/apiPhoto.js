70d8f3fda71882dc703379dbeb9225f1ca893d79
baggio4ever <baggior4ever@gmail.com>
2017-04-15 19:06:45 +0900

    RESTちっくな /api/photo 追加

---
+++ routes/apiPhoto.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+var photoList = [
+    {
+        id: "001",
+        name: "photo001.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo001.jpg"
+    },{
+        id: "002",
+        name: "photo002.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo002.jpg"
+    }
+];
+
+/* GET users listing. */
+router.get('/list', function(req, res, next) {
+  res.json(photoList);
+});
+
+router.get("/:photoId", function(req, res, next){
+    var photo;
+    for (i = 0; i < photoList.length; i++){
+        if (photoList[i].id == req.params.photoId){
+            var photo = photoList[i];
+        }
+    }
+    res.json(photo);
+});
+
+module.exports = router;
+
